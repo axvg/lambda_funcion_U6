2c951fdc5136024a7058e0222e9e1565826faf64
axvg <91773368+axvg@users.noreply.github.com>
2023-01-16 21:19:10 -0500

    Add lambda function

---
+++ lambda_function.py
@@ -0,0 +1,37 @@
+import json
+import mercadopago
+import os
+
+def lambda_handler(event, context):
+
+    sdk = mercadopago.SDK(os.environ["ACCESS_TOKEN"])
+
+    bodyGet = json.loads(event["body"])
+
+    payment_data = {
+        "transaction_amount": float(bodyGet["transaction_amount"]),
+        "token": bodyGet["token"],
+        "installments": int(bodyGet["installments"]),
+        "payment_method_id": bodyGet["payment_method_id"],
+        "payer": {
+            "email": bodyGet["payer"]["email"],
+            "identification": {
+                "type": bodyGet["payer"]["identification"]["type"],
+                "number": bodyGet["payer"]["identification"]["number"]
+            }
+        }
+    }
+
+    payment_response = sdk.payment().create(payment_data)
+    payment = payment_response["response"]
+
+    res = {
+        "status": payment["status"],
+        "status_detail": payment["status_detail"],
+        "id": payment["id"],
+    }
+
+    return {
+        "statusCode": 201,
+        "body": json.dumps(res),
+    }
